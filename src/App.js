898988b5c44846d4890524584905362234483c80
Ali Mohamed <alimohamedali2110@gmail.com>
2023-07-04 14:47:31 +0200

    enhance

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -10,18 +17,71 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1> Far Away 💼</h1>;
+  return <h1>🏝️ Far Away 💼</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSumit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, id: Date.now(), packed: false };
+
+    setDescription('');
+    setQuantity(1);
+
+    console.log(newItem);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSumit}>
       <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
-function PackingList() {
-  return <div className="list">list</div>;
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
+
 function Stats() {
   return (
     <footer className="stats">
