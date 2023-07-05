62f9e149fa172d12e3598e79e77d54b2845ef024
Ali Mohamed <alimohamedali2110@gmail.com>
2023-07-05 11:25:11 +0200

    finish project

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSumit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, id: Date.now(), packed: false };
+
+    onAddItems(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSumit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
